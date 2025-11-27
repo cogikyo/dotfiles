b81207b66a8ff127e8b751b0fa72026e0ecf1864
cullyn <cullyn.newman@gmail.com>
2025-11-27 12:29:38 -0800

    it's not free real estate

---
+++ config/glava/bars_right_rc.glsl
@@ -0,0 +1,32 @@
+#request mod bars_right
+
+/* Window hints */
+#request setfloating  true
+#request setdecorated false
+#request setfocused   false
+#request setmaximized false
+
+#request setopacity "native"
+#request setmirror false
+#request setversion 3 3
+#request setshaderversion 330
+#request settitle "GLava"
+
+#request setgeometry 3720 70 100 2110
+
+#request setbg 00000000
+#request setxwintype "dock"
+#request addxwinstate "below"
+#request setclickthrough true
+#request setsource "auto"
+#request setswap 1
+#request setinterpolate true
+#request setframerate 60
+#request setfullscreencheck false
+#request setprintframes false
+#request setsamplesize 1024
+#request setbufsize 4096
+#request setsamplerate 22050
+#request setforcegeometry false
+#request setforceraised false
+#request setbufscale 1
