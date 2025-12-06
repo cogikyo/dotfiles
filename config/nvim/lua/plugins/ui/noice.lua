046e7936770531d8ea1cd19c40307d12fcc104e3
cullyn <cullyn.newman@gmail.com>
2025-12-06 13:12:49 -0800

    various updates
    fix

--- config/nvim/lua/plugins/ui/noice.lua
+++ config/nvim/lua/plugins/ui/noice.lua
@@ -6,6 +6,16 @@ return {
 		"rcarriga/nvim-notify",
 	},
 	opts = {
+		routes = {
+			{ filter = { event = "msg_show", find = "written$" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", find = "^%d+ more lines?$" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", find = "^%d+ fewer lines?$" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", find = "^%d+ lines? yanked$" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", find = "^E486:" }, opts = { skip = true } },
+			{ filter = { event = "msg_show", find = "^Already at" }, opts = { skip = true } },
+			{ filter = { event = "notify", find = "Config Change Detected" }, opts = { skip = true } },
+		},
 		lsp = {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
