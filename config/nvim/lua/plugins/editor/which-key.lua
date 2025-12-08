be8626eb09f111b2dceb10231f520e717b8b35c7
cullyn <cullyn.newman@gmail.com>
2025-12-07 21:32:17 -0800

    varios changes

--- config/nvim/lua/plugins/editor/which-key.lua
+++ config/nvim/lua/plugins/editor/which-key.lua
@@ -5,13 +5,16 @@ return {
 		local max_width = math.min(math.floor(vim.o.columns * 0.95), 200)
 		return {
 			delay = 350,
-			icons = { mappings = false },
-		win = {
-			border = "rounded",
-			width = max_width,
-			col = 0.5,
-			row = -2,
-		},
+			icons = {
+				mappings = false,
+				keys = {},
+			},
+			win = {
+				border = "rounded",
+				width = max_width,
+				col = 0.5,
+				row = -2,
+			},
 			show_help = false,
 			spec = require("config.keymaps").groups or {},
 		}
