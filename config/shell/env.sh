d314c1b0eadae7473443bc24b63475b174ca56d6
cullyn <cullyn.newman@gmail.com>
2025-12-28 12:44:38 -0800

    better kitty tab management

--- config/shell/env.sh
+++ config/shell/env.sh
@@ -2,7 +2,6 @@
 
 export PATH="$HOME/.cargo/bin:$HOME/.go/bin:$HOME/.local/bin:$PATH"
 export GOPATH="$HOME/.go"
-export GOPRIVATE=git.linecode.net/yos/*
 
 export TERM=xterm-256color
 export PAGER=nvimpager
@@ -13,5 +12,3 @@ export FZF_DEFAULT_OPTS="\
 --color=bg+:#222536,bg:#222536,spinner:#b29ae8,hl:#f8b486 \
 --color=fg:#9db2f4,header:#f8b486,info:#6380ec,pointer:#f8b486 \
 --color=marker:#6380ec,fg+:#8aa4f3,prompt:#6380ec,hl+:#f8b486"
-
-[ -f "$HOME/.anthropic_api_key" ] && export ANTHROPIC_API_KEY=$(cat "$HOME/.anthropic_api_key")
