8e5e1a3ada104d0bd8b910095d6409d5ef3dddb9
cullyn <cullyn.newman@gmail.com>
2026-02-01 14:50:40 -0800

    refactor(xplr): modularize config into separate files
    Split monolithic init.lua into logical modules:
    - config/: general settings, layouts, modes, node types
    - lib/: helper functions
    - plugins/: plugin configuration

---
+++ config/xplr/plugins/init.lua
@@ -0,0 +1,21 @@
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+
+package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/init.lua"
+
+os.execute(string.format("[ -e '%s' ] || git clone '%s' '%s'", xpm_path, xpm_url, xpm_path))
+
+require("xpm").setup({
+	plugins = {
+		"dtomvan/xpm.xplr",
+		"sayanarijit/wl-clipboard.xplr",
+		"sayanarijit/fzf.xplr",
+		"sayanarijit/zoxide.xplr",
+		"sayanarijit/trash-cli.xplr",
+		"Junker/nuke.xplr",
+		"dtomvan/ouch.xplr",
+	},
+	auto_install = true,
+	auto_cleanup = true,
+})
