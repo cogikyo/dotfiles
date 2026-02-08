1b5921504ef6c890a1346c3b292a95b435161036
cullyn <cullyn.newman@gmail.com>
2026-02-08 14:41:45 -0800

    add(nvim/ui): neominimap plugin for floating code minimap

---
+++ config/nvim/lua/plugins/ui/neominimap.lua
@@ -0,0 +1,41 @@
+return {
+	"Isrothy/neominimap.nvim",
+	version = "v3.*",
+	lazy = false,
+	keys = {
+		{ "<leader>nm", "<cmd>Neominimap Toggle<cr>", desc = "Toggle minimap" },
+		{ "<leader>nf", "<cmd>Neominimap Focus<cr>", desc = "Focus minimap" },
+	},
+	init = function()
+		vim.g.neominimap = {
+			auto_enable = true,
+			layout = "float",
+			float = {
+				minimap_width = 10,
+				margin = { right = 0, top = 0, bottom = 0 },
+			},
+			x_multiplier = 4,
+			y_multiplier = 1,
+			delay = 200,
+			click = {
+				enabled = true,
+				auto_switch_focus = false,
+			},
+			search = {
+				enabled = true,
+				mode = "line",
+			},
+			diagnostic = {
+				enabled = true,
+				mode = "line",
+			},
+			git = {
+				enabled = true,
+				mode = "sign",
+			},
+			treesitter = {
+				enabled = true,
+			},
+		}
+	end,
+}
