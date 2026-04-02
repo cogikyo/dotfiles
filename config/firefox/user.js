9bdbf8e34224b4fce1f4a07f294498f404e90b27
cullyn <cullyn.newman@gmail.com>
2026-04-01 21:36:45 -0700

    add(firefox): hide bookmarks toolbar
    - Set browser.toolbars.bookmarks.visibility to "never"

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -3,3 +3,4 @@ user_pref("pdfjs.sidebarViewOnLoad", 0);
 user_pref("extensions.pocket.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
