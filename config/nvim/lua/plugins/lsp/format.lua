831b14c12f738eeb4527bff691b06925ca8e8b7e
cullyn <cullyn.newman@gmail.com>
2026-05-05 15:32:05 -0700

    dx(nvim/lsp): use oxc tooling for configured frontend repos
    - route conform formatting to oxlint and oxfmt when oxlint config is present
    - run oxlint diagnostics for JavaScript and TypeScript buffers in oxc repos

--- config/nvim/lua/plugins/lsp/format.lua
+++ config/nvim/lua/plugins/lsp/format.lua
@@ -1,9 +1,42 @@
+local oxc_root_markers = { ".oxlintrc.json", ".oxlintrc.jsonc", "oxlint.json", "oxlint.jsonc" }
+
+local function oxc_root(bufnr)
+	local filename = vim.api.nvim_buf_get_name(bufnr)
+	return filename ~= "" and vim.fs.root(filename, oxc_root_markers) or nil
+end
+
+local function oxc_cwd(_, ctx)
+	return vim.fs.root(ctx.dirname, oxc_root_markers)
+end
+
+local function js_formatters(bufnr)
+	if oxc_root(bufnr) then
+		return { "oxlint", "oxfmt" }
+	end
+	return { "prettierd" }
+end
+
+local function web_formatters(bufnr)
+	if oxc_root(bufnr) then
+		return { "oxfmt" }
+	end
+	return { "prettierd" }
+end
+
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
 		formatters = {
+			oxfmt = {
+				cwd = oxc_cwd,
+				require_cwd = true,
+			},
+			oxlint = {
+				cwd = oxc_cwd,
+				require_cwd = true,
+			},
 			stylua = {
 				prepend_args = { "--column-width", "120", "--collapse-simple-statement", "FunctionOnly" },
 			},
@@ -13,17 +46,17 @@ return {
 			sh = { "shellharden" },
 			zsh = { "beautysh" },
 			python = { "ruff_format" },
-			css = { "prettierd" },
-			json = { "prettierd" },
-			jsonc = { "prettierd" },
+			css = web_formatters,
+			json = web_formatters,
+			jsonc = web_formatters,
 			lua = { "stylua" },
 			toml = { "taplo" },
-			javascript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			typescript = { "prettierd" },
-			typescriptreact = { "prettierd" },
-			html = { "prettierd" },
-			markdown = { "prettierd" },
+			javascript = js_formatters,
+			javascriptreact = js_formatters,
+			typescript = js_formatters,
+			typescriptreact = js_formatters,
+			html = web_formatters,
+			markdown = web_formatters,
 			yaml = {},
 			go = { "goimports", "gofumpt" },
 			templ = { "templ" },
--- config/nvim/lua/plugins/lsp/lint.lua
+++ config/nvim/lua/plugins/lsp/lint.lua
@@ -3,15 +3,40 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
+		local oxlint_filetypes = {
+			javascript = true,
+			javascriptreact = true,
+			typescript = true,
+			typescriptreact = true,
+		}
+		local oxlint_root_markers = { ".oxlintrc.json", ".oxlintrc.jsonc", "oxlint.json", "oxlint.jsonc" }
+
+		local function oxlint_root(bufnr)
+			local filename = vim.api.nvim_buf_get_name(bufnr)
+			return filename ~= "" and vim.fs.root(filename, oxlint_root_markers) or nil
+		end
+
+		local oxlint = require("lint.linters.oxlint")
+		lint.linters.oxlint = function()
+			return vim.tbl_deep_extend("force", oxlint, {
+				cwd = oxlint_root(0),
+			})
+		end
+
 		lint.linters_by_ft = {
 			zsh = { "zsh" },
 			dockerfile = { "hadolint" },
 		}
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
 			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-			callback = function()
-				if vim.bo.buftype == "" then
-					lint.try_lint()
+			callback = function(args)
+				if vim.bo[args.buf].buftype == "" then
+					vim.api.nvim_buf_call(args.buf, function()
+						if oxlint_filetypes[vim.bo.filetype] and oxlint_root(args.buf) then
+							lint.try_lint("oxlint")
+						end
+						lint.try_lint()
+					end)
 				end
 			end,
 		})
