58b0befa49df02f26a57ab16a14eec31e9dea35d
cullyn <cullyn.newman@gmail.com>
2026-08-01 00:51:07 -0700

    fix lua lsp

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -3,7 +3,10 @@ return {
 	root_markers = { ".luarc.json", ".luarc.jsonc", ".stylua.toml", "stylua.toml", ".git" },
 	settings = {
 		Lua = {
-			workspace = { checkThirdParty = false },
+			workspace = {
+				checkThirdParty = false,
+				ignoreDir = { "config/hypr", "config/xplr" },
+			},
 			telemetry = { enable = false },
 			diagnostics = { globals = { "vim" } },
 			format = { enable = false },
